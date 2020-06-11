fb07b0286a3d8d7967f42de99f5b758ce7ec532d
srinivas <srinivas@sushma>
2020-06-11 09:43:13 +0530

    Done updation in Example1 file

--- Milestone1/src/com/wipro/languagebasics/Example1.java
+++ Milestone1/src/com/wipro/languagebasics/Example1.java
@@ -1,5 +1,11 @@
 package com.wipro.languagebasics;
 
 public class Example1 {
+	public static void main(String[] args) {
+		int a=Integer.parseInt(args[0]);
+		System.out.println(a);
+		
+		
+	}
 
 }
